70cae52e1ac282cd318d2a1aeaad56c023a11cfa
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-12-10 19:07:51 -0500

    Corrected the db

--- models/volunteer.js
+++ models/volunteer.js
@@ -1,8 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
     const Volunteer = sequelize.define("Volunteer", {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
-        phone: DataTypes.INTEGER,
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone: {
+            type: DataTypes.INTEGER(12),
+            allowNull: false,
+        },
     });
 
     return Volunteer;
