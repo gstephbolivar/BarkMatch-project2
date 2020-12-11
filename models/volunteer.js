7a5837d24c37ba1a31bb4c6c4ccb92a9b7d6cced
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-12-10 19:08:12 -0500

    Set constraints to volunteer model

--- models/volunteer.js
+++ models/volunteer.js
@@ -1,18 +1,21 @@
 module.exports = function (sequelize, DataTypes) {
-    const Volunteer = sequelize.define("Volunteer", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        phone: {
-            type: DataTypes.INTEGER(12),
-            allowNull: false,
-        },
-    });
+  const Volunteer = sequelize.define("Volunteer", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    phone: {
+      type: DataTypes.INTEGER(12),
+      allowNull: false,
+    },
+  });
 
-    return Volunteer;
+  return Volunteer;
 };
