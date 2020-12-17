e452999f5325a2c86a39d50b677f15b9c297e88d
Melanie Bostwick <mbostwick1@gmail.com>
2020-12-17 18:38:23 -0500

    fixed logged in and log out in nav

--- db/dogSeeds.sql
+++ db/dogSeeds.sql
@@ -1,2 +1,4 @@
+USE doggo_db;
+
 INSERT INTO Dogs (name, age, breed, gender, size, energy_level, bio, available, img_path, createdAt, updatedAt) VALUES ('Oliver', 3, 'pomsky', 'Male', 'Small', 'Moderate', "The most lovable pup you'll see", 1,'/assets/images/dogs/Dog-1.jpg', Now(), Now()), ('Andre', 8, 'Australian Cattle', 'Male', 'Medium', 'Low', "After a tough beginning, Andre just loves to hang out and snuggle with his hoomans", 1,'/assets/images/dogs/Dog-2.jpg', Now(), Now()), ('Nadia', 8, 'Chihuahua Mix', 'Female', 'Small', 'High', "Nadia just wants to play! Great on the leash but will want to walk for hours if she could", 1, '/assets/images/dogs/Dog-3.jpg', Now(), Now());
 
