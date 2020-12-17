f89b20d691492bb00278195fdd88a5644d3ed6c2
Rashawn Raiford <raiford87@gmail.com>
2020-12-16 21:18:48 -0500

    Added dashboard login capability

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models");
+const isAuthenticated = require('../config/isAuthenticated');
 
 // HTML routes
 
 // Shelter dashboard home page
-router.get("/dashboard", (req, res) => {
-  db.Dogs.findAll()
-    .then((dogs) => {
-      res.render("dashboard", { dogs: dogs });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).render("errorPage");
-    });
+router.get("/dashboard", isAuthenticated, (req, res) => {
+    db.Dogs.findAll()
+      .then((dogs) => {
+        res.render("dashboard", { dogs: dogs });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).render("errorPage");
+      });
 });
 
 // Page to add a dog
-router.get("/dashboard/add", (req, res) => {
+router.get("/dashboard/add", isAuthenticated, (req, res) => {
   res.render("addDog");
 });
 
 //Page to edit a dog
-router.get("/dashboard/edit/:id", (req, res) => {
+router.get("/dashboard/edit/:id", isAuthenticated, (req, res) => {
   db.Dogs.findOne({where: {id: req.params.id}})
   .then(dog => {
     res.render("editDog", dog._previousDataValues);
