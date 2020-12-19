03f2d6fc70933f9ebdbc0d47df279768d334836b
Stephany Bolivar <g.stephanybolivar@gmail.com>
2020-12-18 22:33:24 -0500

    Comment for server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// NPM packages required and other files
 const express = require('express');
 const handlebars = require("handlebars");
 const {allowInsecurePrototypeAccess} = require("@handlebars/allow-prototype-access");
@@ -6,10 +7,13 @@ const fileUpload = require('express-fileupload');
 const session = require("express-session");
 const passport = require("./config/passport");
 
+// Port set
 const PORT = process.env.PORT || 8080;
 
+// Create instance of express
 const app = express();
 
+// middleware
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
@@ -21,9 +25,11 @@ app.use(passport.session());
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
+// handlebars middleware
 app.engine("handlebars", exphbs({defaultLayout: "main", handlebars: allowInsecurePrototypeAccess(handlebars)}));
 app.set("view engine", "handlebars");
 
+// Routes
 const dogsController = require('./controllers/dogsController.js');
 app.use(dogsController);
 
@@ -36,6 +42,7 @@ app.use(dashboardController);
 const loginController = require('./controllers/loginController.js');
 app.use(loginController);
 
+// Syncing sequelize models and then starting express app
 // db.sequelize.sync({force: true}).then(() => {
 db.sequelize.sync().then(() => {
     app.listen(PORT, () => {
