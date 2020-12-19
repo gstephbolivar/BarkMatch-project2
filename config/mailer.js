eb5d18004abf12cb50503cfe5f4c01db3d90da7a
Rashawn Raiford <raiford87@gmail.com>
2020-12-18 21:38:41 -0500

    Fixed email template

--- config/mailer.js
+++ config/mailer.js
@@ -2,25 +2,26 @@ var nodemailer = require('nodemailer');
 var path = require('path');
 
 module.exports = function(dog, volunteer){
-    const template = `<h1>Hello, ${volunteer.name}!</h1></br></br>Thank you for your interest in walking ${dog.name}.</br>
-    If you have any questions of concerns please contact us.<br><br></br><img src="cid:adogpic" style="width:200px;height:300px"/>`;
+    const template = `<h1>Hello, ${volunteer.name}!</h1></br></br>Thank you for using BarkMatch. This is your confirmation for signing up to walk ${dog.name}.</br>
+    If you have any questions of concerns please contact us.<br><br><br><img src="cid:adogpic" style="width:200px;height:200px;display:block;margin-left:auto;margin-right:auto"/>`;
 
+    //Create transport with service to use for email
     var transporter = nodemailer.createTransport({
         service: 'yahoo',
         host: 'smtp.mail.yahoo.com',
         port: 587,
         secure: false,
         auth: {
-          user: 'rashawn2530',
-          pass: 'ptvsjdxkrwxovmvu'
+          user: 'barkmatch',
+          pass: 'jtglkrqexcrvkeww'
         }
       });
 
-    //   const pp = path.join(__dirname, "../public", dog.img_path)
+      //Create options for the email
       var mailOptions = {
-        from: 'BarkMatch <rashawn2530@yahoo.com>',
+        from: 'BarkMatch <barkmatch@yahoo.com>',
         to: volunteer.email,
-        subject: 'BarkMatch Confirmation',
+        subject: 'Confirmation',
         html: template,
         attachments: [{
             filename: path.basename(dog.img_path),
@@ -29,6 +30,7 @@ module.exports = function(dog, volunteer){
         }]
       };
       
+      //Send the email
       transporter.sendMail(mailOptions, function(error, info){
         if (error) {
           console.log(error);
